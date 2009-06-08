d6b70f4795165813fcc2290d93291f70ba470ba6
Ochirkhuyag <ochkoo@gmail.com>
2009-06-09 03:28:51 +0900

    more mobile displays

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -50,7 +50,6 @@ class SessionsController < ApplicationController
     new_cookie_flag = (params[:remember_me] == "1")
     handle_remember_cookie! new_cookie_flag
     redirect_back_or_default(root_path)
-    flash[:notice] = "Logged in successfully"
   end
 
   def note_failed_signin
