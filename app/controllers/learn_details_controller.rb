2792323e86bf6981ce2936cc3b50e78347431bfb
Ochirkhuyag <ochkoo@gmail.com>
2009-06-09 10:22:01 +0900

    list for learn_details

--- app/controllers/learn_details_controller.rb
+++ app/controllers/learn_details_controller.rb
@@ -1,5 +1,6 @@
 class LearnDetailsController < ApplicationController
   before_filter :login_required
+  require_role "admin", :except => :update
   def update
     learn_detail = LearnDetail.find(params[:id])
     learn_detail.update_attribute(:answer, params[:answer])
@@ -9,6 +10,10 @@ class LearnDetailsController < ApplicationController
       redirect_to :controller => :memo, :action => :show, :id => learn_detail.word_id
     end
     
+  end
+  
+  def index
+    @learn_details = LearnDetail.all
   end
 
 end
